50b682c96c92078997c842cee6148cbc6a132c5d
Selim-Dev <alisleem811@gmail.com>
2022-04-15 15:01:59 +0200

    ali

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -5,14 +5,13 @@ const NavigationDots = ({ active }) => {
     <div className="app__navigation">
       {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map(
         (item, index) => (
+          // eslint-disable-next-line jsx-a11y/anchor-has-content
           <a
             href={`#${item}`}
             key={item + index}
             className="app__navigation-dot"
             style={active === item ? { backgroundColor: '#313bac' } : {}}
-          >
-            .
-          </a>
+          ></a>
         )
       )}
     </div>
